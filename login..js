4d6ea62707566a94972120e086f4dab3d67daea0
raphique-code <raphiquemcmaster@gmail.com>
2021-07-04 18:47:20 +0800

    adding folding

---
+++ login..js
@@ -0,0 +1,82 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import {Formik} from 'formik';
+
+import {Octicons} from '@expo/Vector-icon';
+
+import {Colors} from './styles'
+
+import {
+    StyledContainer,
+    InnerContainer,
+    PageLogo,
+    PageTitle,
+    Subtitle,
+    StyledFormArea,
+    LeftIcon,
+    StyledInputLabel, 
+    StyledTextInput,
+    RightIcon,
+    colors
+//the file is located in one level above the current folder
+} from './styles'
+import {view} from 'react-native';
+const {primary} = Colors;
+const Login = () =>{
+    return(
+        <StyledContainer>
+            <StatusBar style="dark"/>
+            <InnerContainer>
+                <PageLogo resizeMode="cover"source={require('./assets/logoV-full.png')} />
+                <PageTitle> nathan and deliver
+                </PageTitle>
+                <Subtitle>Account Login</Subtitle>
+                <Formik
+                    initialValues={{email: '', password: ''}}
+                    onSubmit={(values) =>{console.log(values);
+                    }}
+
+                >{({handleChange,handleBlur,handleSubmit,values })=> (
+                    <StyledFormArea>
+                        <MyTextInput
+                            label="Email Address"
+                            icon="mail"
+                            placeholder="Nathan@gmail.com"
+                            placeholderTextColor={darkLight}
+                            onChangeText={handleChange('email')}
+                            onBlur={handleChange('email')}
+                            Value={values.email}
+                            keyboardTypes="email-address"
+                        />
+
+                        <MyTextInput
+                            label="pasword"
+                            icon="lock"
+                            placeholder="********"
+                            placeholderTextColor={darkLight}
+                            onChangeText={handleChange('password')}
+                            onBlur={handleChange('password')}
+                            Value={values.password}
+                            secureTextEntry={true}
+                        />
+                    </StyledFormArea>)}
+                </Formik>
+            </InnerContainer>
+        </StyledContainer>
+
+    );
+}
+
+const MyTextInput = ({label, icon, ...props}) => {
+    return(
+        <View>
+            <LeftIcon>
+                <Octicons name={icon} size={30} color={primary}/>
+            </LeftIcon>
+            <StyledInputLabel>{label}</StyledInputLabel>
+            <StyledTextInput {...props}/>
+        </View>
+    )
+
+}
+export default Login;
---
+++ styles.js
@@ -0,0 +1,101 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import {view} from 'react-native';
+import constants from 'expo-constants';
+import styled from 'styled-components/native';
+
+const statusBarHeight = constants.statusBarHeight;
+// colors
+export const Colors= {
+  primary: '#DA7737',
+  background: '#1A130E',
+  offWhite: '#F4ECE7',
+
+};
+
+const {primary,background,offWhite} = Colors;
+
+
+
+
+export const StyledContainer = styled.View`
+  flex: 1;
+  padding: 25px;
+  padding-top: ${statusBarHeight + 10}px;
+  background-color: ${background};
+`
+
+export const InnerContainer = styled.View`
+  flex: 1;
+  width: 100%;
+  align-item: center;
+  
+
+`;
+
+export const PageLogo = styled.Image`
+  width: 500px;
+  height: 500px;
+  postion: absolute;
+  
+
+`;
+
+export const PageTitle = styled.Text`
+  font-size: 30px;
+  text-align: center;
+  font-weight: bold;
+  color: ${primary};
+  padding: 10px;
+`;
+
+export const SubTitle = styled.Text`
+  font-size: 18px;
+  margin-bottom: 20px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  color: ${primary}
+`;
+
+export const StyledFormArea = styled.View`
+width: 90%;
+
+`;
+
+export const StyledTextInput = styled.TextInput`
+  background-color: ${primary};
+  padding: 15px;
+  padding-left: 55px;
+  border-radius: 5px;
+  font-size: 16;
+  height: 60;
+  margin-vertical: 3px;
+  margin-bottom: 10px;
+  color: ${offWhite};
+
+
+`;
+ 
+export const StyledTextInputLabel = styled.Text`
+  color: ${primary};
+  font-size: 13px;
+  text-align: left;
+
+`;
+
+export const LeftIcon = styled.View`
+  left: 15px;
+  top: 38px;
+  postion: absolute;
+  z-index: 1;
+
+`;
+export const RightIcon = styled.TouchableOpacity`
+  left: 15px;
+  top: 38px;
+  postion: absolute;
+  z-index: 1;
+
+`;
+
